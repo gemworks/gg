fe02c3850d7eee612f53d2c1ccd44dab277a336a
ilmoi <iljamoi@protonmail.com>
2021-12-29 22:39:14 +0200

    temp redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,14 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
+    name: "gemworks.gg",
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    beforeEnter: (to, from, next) => {
+      window.location.href = 'https://docs.gemworks.gg'
+    }
   },
 ];
 
